c6c64d0affcefa7d2a0e8db8c6ab26fc737e5da1
Levi <levilkok@gmail.com>
2020-12-03 11:06:24 +0100

    NN full rollout

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ df = utils.create_summation(df)
 df = utils.df_add_dates(df)
 df = utils.df_drop_columns_and_outliers(df)
 df = utils.insert_past_revenues(df)
-df = utils.binning(df, bin_size)
+#df = utils.binning(df, bin_size)
 
 df_weather = weather.get_weather()
 
@@ -113,15 +113,59 @@ for i, x in post_x.iloc[0:20].iterrows():
     outputs.append(output//100)
     targets.append(target//100)
     
+
+#TOTAL ROLLOUT
+
+store_id = 3
+
+post_x, post_y = utils.create_train_test(df_post, test = False, store_id = store_id)
+revenue_array = utils.get_past_revenue_array(df, store_id, date, 30)
+
+output_rollout = []
+
+day = datetime.strptime(date, "%Y-%m-%d")
+
+for i, x in post_x.iloc[0:20].iterrows():
     
+    target = post_y.loc[i]   
+    
+    r_1 = revenue_array[-1]
+    r_2 = revenue_array[-2]
+    r_7 = revenue_array[-7]
+    r_month = revenue_array[-28]
+    
+    x.revenue_1daysago = r_1
+    x.revenue_2daysago = r_2
+    x.revenue_lastweek = r_7
+    x.revenue_lastmonth = r_month
+    
+    output = model.model.predict(x.astype(float).to_numpy().reshape(1,-1))
+    
+    output = int(output.item())
+
+    output_rollout.append(output//100)
+    revenue_array.append(output)
+
+    del revenue_array[0]
+    day = day + dt.timedelta(days=1)
+    
+    print(f"{day.strftime('%Y-%m-%d')} - {day.strftime('%A')} - {target/100} - {output/100}")
+
+
+
 plt.bar(np.arange(len(outputs)) - 0.25, outputs, 0.25, label = 'output 1 day rollout', color = 'dodgerblue')
+plt.bar(np.arange(len(outputs)), output_rollout, 0.25, label = 'output total rollout', color = 'aqua')
 plt.bar(np.arange(len(outputs)) + 0.25, targets, 0.25, label = 'target', color = 'lightcoral')
 
+plt.ylim([500, 1800])
 plt.legend()
 plt.title('1 day vs total rollout for store 3 in January 2020')
 plt.xlabel('days since 03 jan 2020')
 plt.ylabel('euro')
 plt.show()
+
+
+
 
 
 
