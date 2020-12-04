c3e7674c7301ed82dfd694459dfada4c503ef408
Levi <levilkok@gmail.com>
2020-12-04 10:04:18 +0100

    (no branch): Auto stash before rebase of "main"

--- main.py
+++ main.py
@@ -78,9 +78,6 @@ df = utils.remove_nan_values(df)
 date = '2020-01-01'
 
 
-scaler = MinMaxScaler()
-df_scaled = df.copy()
-df_scaled[df_scaled.columns] = scaler.fit_transform(df_scaled[df_scaled.columns])
 df['store_id'] = df.index.get_level_values('store_id')
 
 df_pre, df_post = utils.split_on_date(df, date)
@@ -88,18 +85,17 @@ df_pre, df_post = utils.split_on_date(df, date)
 train_x, train_y, test_x, test_y = utils.create_train_test(df_pre)
 
 model = FullyConnected(train_x.shape[1])
-model.model.fit(train_x.astype(float), train_y, validation_data = (test_x.astype(float), test_y), epochs = 10)
+model.model.fit(train_x.astype(float), train_y, validation_data = (test_x.astype(float), test_y), epochs = 50)
 output = model.predict(test_x.astype(float))
 
 model.test(test_x.astype(float), test_y)
 
-
-#1 DAY ROLLOUT
+#NN
 
 store_id = 3
 post_x, post_y = utils.create_train_test(df_post, test = False, store_id = store_id)
 
-outputs = []
+outputs_gradient_nn = []
 targets = []
 
 for i, x in post_x.iloc[0:20].iterrows():
@@ -110,7 +106,7 @@ for i, x in post_x.iloc[0:20].iterrows():
     
     output = int(output.item())
 
-    outputs.append(output//100)
+    outputs_gradient_nn.append(output//100)
     targets.append(target//100)
     
 
@@ -139,6 +135,7 @@ for i, x in post_x.iloc[0:20].iterrows():
     x.revenue_lastweek = r_7
     x.revenue_lastmonth = r_month
     
+<<<<<<< Updated upstream
     output = model.model.predict(x.astype(float).to_numpy().reshape(1,-1))
     
     output = int(output.item())
@@ -156,15 +153,40 @@ for i, x in post_x.iloc[0:20].iterrows():
 plt.bar(np.arange(len(outputs)) - 0.25, outputs, 0.25, label = 'output 1 day rollout', color = 'dodgerblue')
 plt.bar(np.arange(len(outputs)), output_rollout, 0.25, label = 'output total rollout', color = 'aqua')
 plt.bar(np.arange(len(outputs)) + 0.25, targets, 0.25, label = 'target', color = 'lightcoral')
+=======
+plt.bar(np.arange(len(outputs_gradient_nn)) - 0.25, outputs_gradient_nn, 0.25, label = 'output 1 day rollout', color = 'dodgerblue')
+plt.bar(np.arange(len(outputs_gradient_nn)) + 0.25, targets, 0.25, label = 'target', color = 'lightcoral')
+>>>>>>> Stashed changes
 
 plt.ylim([500, 1800])
 plt.legend()
-plt.title('1 day vs total rollout for store 3 in January 2020')
+plt.title('NN - 1 day rollout for store 3 in January 2020')
+plt.ylim([600, 1800])
 plt.xlabel('days since 03 jan 2020')
 plt.ylabel('euro')
 plt.show()
 
 
+model = GradientBoosting()
+model.fit(train_x, train_y)
+model.test(test_x, test_y)
+
+#GRADIENT 
+
+outputs_gradient = []
+targets = []
+
+for i, x in post_x.iloc[0:20].iterrows():
+
+    target = post_y.loc[i]   
+
+    x_vec = model.toNumpyVector(x)
+    output = model.predict(x_vec)
+
+    output = int(output.item())
+
+    outputs_gradient.append(output//100)
+    targets.append(target//100)
 
 
 
